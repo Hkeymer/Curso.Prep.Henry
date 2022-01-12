de8ffa1dfc52b33995bfdee51d7f16cbe90c6371
keimer cordoba <97525425+Hkeymer@users.noreply.github.com>
2022-01-12 10:28:45 -0500

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -52,18 +52,27 @@ function multiplica(x, y) {
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  return x / y;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( x === y ){
+    return true;
+  }
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (str1.length=== str2.length){
+    return true;
+  }
+  false
 }
 
 function menosQueNoventa(num) {
