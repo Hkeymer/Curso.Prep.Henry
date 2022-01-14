10a6000c587ed4e7c73c53969cf5365f4c777392
keimer cordoba <97525425+Hkeymer@users.noreply.github.com>
2022-01-14 13:12:11 -0500

    Tarea Terminada

---
+++ 02-JS-I/Texto.js
@@ -0,0 +1,19 @@
+// Variables.
+// Es donde se almacenan los valores o tados, tales como:
+// las strinsg, Numbers, Booleanos, Null,Undefined.
+// las cuales estan espuestas a ciertos cambios que se pueden presentar a lo largo del tiempo. 
+//Strings.
+//Las strings son conocidas como cadenas de texto y siempre que las querramos incluir en nuestro codigo debemos penerlas entre comillas
+// Ejemplo: "hola mundo"
+// funciones.
+//Fracmentos de codigos que podemos reciclar cuantas veces nosotros queramo y podemos 
+//darle un dinamismo a cada una de ellas, para que hagan cosas por nosotros.
+//argumentos.
+//Son parametros que representan ciertos valores en un codigo.
+//return
+// ES declaración  que nos retorna o nos devuele un valor.
+//Una declaración es una construcción del lenguaje que asocia un nombre con una entidad.
+// ejemplo: if
+// if es una declaracion que se usa como condional y en español significa si.
+// Valores booleanos.
+// dentro de los valores booleanos encontramos a true y false, en donde true significa verdadero y false, falso.
--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -231,10 +231,9 @@ function esVocal(letra){
   if( letra.length > 1 ){
     return "Dato incorrecto"
   }
-  if( letra === a || letra === e || letra === i || letra === o || letra === u ){
+  if( letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u" ){
     return "Es vocal"
   }
-
 }
 
 
